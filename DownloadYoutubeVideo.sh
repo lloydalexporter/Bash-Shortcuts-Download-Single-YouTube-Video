0a9ef4b476d87bd73d3ca2e839fda04f2019fab8
Lloyd Alex Porter <48352202+lloydalexporter@users.noreply.github.com>
2022-02-03 21:59:13 +0000

    Fixed?

--- DownloadYoutubeVideo.sh
+++ DownloadYoutubeVideo.sh
@@ -38,7 +38,7 @@ videoDirectory="$downloadsFolder/$videoTitle"
 
 /bin/sleep 3 && echo "Downloading the YT video"
 # Download the youtube video.
-/opt/homebrew/bin/youtube-dl -f bestvideo+bestaudio "$videoURL" -o "$videoDirectory/$videoTitle.%(ext)" & wait
+/opt/homebrew/bin/youtube-dl -f bestvideo+bestaudio "$videoURL" -o "$videoDirectory/$videoTitle.%(ext) --merge-output-format mp4" & wait
 
 /bin/sleep 3 && echo "Getting the video title"
 # Get the videos full title.
