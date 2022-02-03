0397411d8db42b0c8984d844c8c0fd6218751a6e
Lloyd Alex Porter <48352202+lloydalexporter@users.noreply.github.com>
2022-02-03 22:25:45 +0000

    Bracketted once more

--- DownloadYoutubeVideo.sh
+++ DownloadYoutubeVideo.sh
@@ -38,7 +38,7 @@ videoDirectory="$downloadsFolder/$videoTitle"
 
 /bin/sleep 3 && echo "Downloading the YT video"
 # Download the youtube video.
-/opt/homebrew/bin/youtube-dl -f bestvideo+bestaudio "$videoURL" -o "$videoDirectory/$videoTitle.%(ext) --merge-output-format mp4 --ffmpeg-location /opt/homebrew/bin/ffmpeg" & wait
+$(/opt/homebrew/bin/youtube-dl -f bestvideo+bestaudio "$videoURL" -o "$videoDirectory/$videoTitle.%(ext)s")     & wait
 
 /bin/sleep 3 && echo "Getting the video title"
 # Get the videos full title.
