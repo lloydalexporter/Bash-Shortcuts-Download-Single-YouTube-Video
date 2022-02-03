0bda547cf525ebfff5a2f0b9417ddf2034279def
Lloyd Alex Porter <48352202+lloydalexporter@users.noreply.github.com>
2022-02-03 20:23:22 +0000

    Replacing Square Brackets with test

--- DownloadYoutubeVideo.sh
+++ DownloadYoutubeVideo.sh
@@ -1,5 +1,5 @@
 # Check if we have any parameter supplied.
-if [ $# -eq 0 ]; then
+if test $# -eq 0 ; then
     # We don't have any parameters supplied: Ask until we do.
     videoURL=
 	while [[ $videoURL == "" ]]
@@ -9,7 +9,7 @@ if [ $# -eq 0 ]; then
 		videoURL=
 		read -p "" videoURL
         # If the input is a YouTube link, then continue, else we loop again.
-        if [[ "$videoURL" != *"youtu.be"* ]] | [[ "$videoURL" != *"youtube.com"* ]]
+        if test "$videoURL" != *"youtu.be"* | "$videoURL" != *"youtube.com"*
         then
             echo
             echo This URL is not a youtube video.
@@ -20,7 +20,7 @@ else
     # We do have a parameter supplied:
     videoURL="$1"
     # If the input is a YouTube link, then continue, else exit.
-    if [[ "$videoURL" != *"youtu.be"* ]] | [[ "$videoURL" != *"youtube.com"* ]]
+    if test "$videoURL" != *"youtu.be"* | test "$videoURL" != *"youtube.com"*
     then
         echo
         echo Not a youtube video, try again with a correct link.
@@ -28,48 +28,25 @@ else
     fi
 fi
 
-echo A
-
 # Get video title and format it.
 videoTitle=$(/opt/homebrew/bin/youtube-dl -f bestvideo+bestaudio "$videoURL" -o "%(title)s.%(ext)s" --get-title)
 videoTitle=$(echo $videoTitle | sed 's/["/]//g')
-
-echo B
 
 # Set videoDownload Directory.
 downloadsFolder="/Users/$(whoami)/Downloads"
 videoDirectory="$downloadsFolder/$videoTitle"
 
-echo C
-
 # Download the youtube video.
 $('/opt/homebrew/bin/youtube-dl' -f bestvideo+bestaudio "$videoURL" -o "$videoDirectory/$videoTitle.%(ext)s")
-
-echo D
 
 # Get the videos full title.
 videoFullTitle=$(ls "$videoDirectory" | grep -e "$videoTitle")
 
-echo E
-
 # Check if the video is already an MP4 file, if not then convert it to one.
 test -f "$videoDirectory/$videoTitle.mp4" || '/opt/homebrew/bin/ffmpeg' -i "$videoDirectory/$videoFullTitle" "$videoDirectory/$videoTitle.mp4" ;
-
-echo F
 
 # Move the MP4 video file to the Downloads folder.
 /bin/mv "$videoDirectory/$videoTitle.mp4" "$downloadsFolder"
 
-echo G
-
 # Remove the directory with any undeleted files.
 /bin/rm -dr "$videoDirectory"
-
-echo H
-
-
-if test 1 -eq 1 ; then
-    echo True
-else
-    echo False
-fi
