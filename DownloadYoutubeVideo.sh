2f6b3e14612a0552cb21f748859dccecbafa2a64
Lloyd Alex Porter <48352202+lloydalexporter@users.noreply.github.com>
2022-02-09 16:38:23 +0000

    Fixing the change I just did lol

--- DownloadYoutubeVideo.sh
+++ DownloadYoutubeVideo.sh
@@ -8,7 +8,7 @@
 # ! - Constants - ! #
 YouTubeCmd='/opt/homebrew/bin/youtube-dl'
 # ffmpegCmd='/opt/homebrew/bin/ffmpeg'
-ffmpegCmd='/opt/homebrew/Cellar/ffmpeg/*/bin/ffmpeg'
+ffmpegCmd='/opt/homebrew/Cellar/ffmpeg/5.0/bin/ffmpeg'
 downloadsFolder="/Users/$(whoami)/Downloads"
 
 
