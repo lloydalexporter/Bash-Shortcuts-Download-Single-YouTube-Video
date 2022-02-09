6e19bba1268e49513076cc0ea1d9f0f691698dc7
Lloyd Alex Porter <48352202+lloydalexporter@users.noreply.github.com>
2022-02-09 14:38:30 +0000

    Prevent Deletion...

--- DownloadYoutubeVideo.sh
+++ DownloadYoutubeVideo.sh
@@ -68,6 +68,6 @@ fi
 /bin/mv "$videoDirectory/$videoTitle.mp4" "$downloadsFolder/$videoTitle.mp4" #& wait
 
 # Remove the directory with any undeleted files.
-/bin/rm -dr "$videoDirectory" #& wait
+# /bin/rm -dr "$videoDirectory" #& wait
 
 echo Done
